3664b6869daa056b1c4ca37b205db41bd57d7e0c
Alejosp <mona@ubuntu.(none)>
2013-10-22 11:10:43 -0500

    agregadas unas signaturas

---
+++ Tarea6/skel/evolve.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "masa.h"
+
+void carga(char *filename);//carga la configuracion del choque.
+void guardar(MASA *estrellas, char *filename);//guarda el estado pasado.
