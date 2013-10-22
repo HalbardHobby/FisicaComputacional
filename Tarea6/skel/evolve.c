2f831b406a4c7790da15ff34b999a3034157f07e
Alejosp <mona@ubuntu.(none)>
2013-10-22 13:18:03 -0500

    Terminadas las signaturas... creo.

--- Tarea6/skel/evolve.c
+++ Tarea6/skel/evolve.c
@@ -4,3 +4,13 @@
 
 void carga(char *filename);//carga la configuracion del choque.
 void guardar(MASA *estrellas, char *filename);//guarda el estado pasado.
+float func(float x, float y);//retorna la función;
+void runge_kutta(MASA *estrella);//ejecuta runge kutta sobre los datos.
+
+int main(int argc, char **argv){
+  if(argc!=2){
+    printf("Que pasa que no pasan las cosas como son!?");
+    return -1;
+  }
+  return 0;
+}
