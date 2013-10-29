3b03baf01b2e480ceac7c05e688aeaa367ec10d7
Alejosp <mona@ubuntu.(none)>
2013-10-29 09:02:52 -0500

    terminada signatura

--- Tarea6/skel/evolve.c
+++ Tarea6/skel/evolve.c
@@ -4,8 +4,9 @@
 
 void init();//iniciliza el programa con las 100 masas
 float calc_fuerza(float dist);//calcula la fuerza con base en la distancia 
-void configurar masa(MASA *estrella, float velang);//configura la masa
-void movimiento();//cambia el movimiento de las estrellas con respecto al centro de masa
+void configurar_masa(MASA *estrella, float velang);//configura la masa
+void movimiento(MASA *gal);//cambia el movimiento de las estrellas con respecto al centro de masa
+void evolve(MASA *estrella);//calcula la posición de la estrella en la siguiente iteración.
 
 int main(int argc, char **argv){
   if(argc != 5){
@@ -15,20 +16,3 @@ int main(int argc, char **argv){
   printf("%lf %lf\n", C_GRAV, M_CENT );
   return 0;
 }
-
-void carga(char *filename);//carga la configuracion del choque.
-void guardar(MASA *estrellas, char *filename);//guarda el estado pasado.
-<<<<<<< HEAD
-float func(float x, float y);//retorna la función;
-void runge_kutta(MASA *estrella);//ejecuta runge kutta sobre los datos.
-
-int main(int argc, char **argv){
-  if(argc!=2){
-    printf("Que pasa que no pasan las cosas como son!?");
-    return -1;
-  }
-  return 0;
-}
-=======
-
->>>>>>> de543fe9952800e5df34a9b5373d639aecf16fcd
