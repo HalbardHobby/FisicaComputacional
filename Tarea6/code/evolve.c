d1d5b4c83879c7086953892621520f0907301d84
Alejosp <mona@ubuntu.(none)>
2013-11-05 15:32:02 -0500

    otras cosas adicionales

--- Tarea6/code/evolve.c
+++ Tarea6/code/evolve.c
@@ -9,6 +9,9 @@ void configurar_masa(MASA *estrella, float velang);//configura la masa
 void movimiento(MASA *gal);//cambia el movimiento de las estrellas con respecto al centro de masa
 void evolve(MASA *estrella);//calcula la posición de la estrella en la siguiente iteración.
 
+const int pos_c1=0;
+const int pos_c2=120;
+
 int main(int argc, char **argv){
   if(argc != 2){
     printf("Como conquistar la galaxia sin tener idea donde va?");
@@ -16,26 +19,26 @@ int main(int argc, char **argv){
   }
 
   //contando las lineas del archivo
-  FILE *mapa=fopen(argv[1],'r');
+  FILE *mapa=fopen(argv[1],"r");
   if(mapa==0){//mirando que el archivo este bien.
     printf("Algo malo ocurrio.\n");
     return -1;
   }
   char c=0;
   int cont=0;
-  while(C!=EOF){
-    c=fgetc();
+  while(c!=EOF){
+    c=fgetc(mapa);
     if(c=='\n')
       cont++;
   }
 
   //cargando el archivo
   fclose(mapa);
-  mapa=fopen(argv[1],'r');
+  mapa=fopen(argv[1],"r");
   MASA *gal=malloc(cont*sizeof(MASA));
-  int i =0
+  int i =0;
   for(i;i<cont;i++)
-    fscanf(mapa, "%d %f %f %f %f", gal[i]->id, gal[i]->x, gal[i]->y, gal[i]->vx, gal[i]->vy);
+    fscanf(mapa, "%d %f %f %f %f", &gal[i].id, &gal[i].x, &gal[i].y, &gal[i].vx, &gal[i].vy);
 
   return 0;
 }
--- Tarea6/code/masa.h
+++ Tarea6/code/masa.h
@@ -5,13 +5,15 @@
 const float C_GRAV=4.52*pow(10,-4);
 const float M_CENT=pow(10,12);
 
-typedef struct {//define una masa en el espacio.
+typedef struct masa{//define una masa en el espacio.
   int id;//el identificador númerico de la masa.
   //las posiciones en cada una de las coordenadas.
   float x;
   float y;
+  float z;
   //las velocidades de la masa en cada uno de los ejes.
   float vx;
   float vy;
+  float vz;
 } MASA;
 #endif
