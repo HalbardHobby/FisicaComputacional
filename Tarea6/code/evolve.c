5f19145fa1acf291de43cddae67bf03d86754962
Alejosp <mona@ubuntu.(none)>
2013-11-05 15:05:21 -0500

    carga de archivo

--- Tarea6/code/evolve.c
+++ Tarea6/code/evolve.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "masa.h"
+#include "second_order_ODE.h"
 
 void init();//iniciliza el programa con las 100 masas
 float calc_fuerza(float dist);//calcula la fuerza con base en la distancia 
@@ -9,10 +10,28 @@ void movimiento(MASA *gal);//cambia el movimiento de las estrellas con respecto 
 void evolve(MASA *estrella);//calcula la posición de la estrella en la siguiente iteración.
 
 int main(int argc, char **argv){
-  if(argc != 5){
+  if(argc != 2){
     printf("Como conquistar la galaxia sin tener idea donde va?");
     return -1;
   }
-  printf("%lf %lf\n", C_GRAV, M_CENT );
+
+  //contando las lineas del archivo
+  FILE *mapa=fopen(argv[1],'r');
+  char c=0;
+  int cont=0;
+  while(C!=EOF){
+    c=fgetc();
+    if(c=='\n')
+      cont++;
+  }
+
+  //cargando el archivo
+  fclose(mapa);
+  mapa=fopen(argv[1],'r');
+  MASA *gal=malloc(cont*sizeof(MASA));
+  int i =0
+  for(i;i<cont;i++){
+    fscanf(mapa, "%d %f %f %f %f", gal[i]->id, gal[i]->x, gal[i]->y, gal[i]->vx, gal[i]->vy);
+
   return 0;
 }
--- Tarea6/code/masa.h
+++ Tarea6/code/masa.h
@@ -5,7 +5,7 @@
 const float C_GRAV=4.52*pow(10,-4);
 const float M_CENT=pow(10,12);
 
-typedef struct masa{//define una masa en el espacio.
+typedef struct {//define una masa en el espacio.
   int id;//el identificador númerico de la masa.
   //las posiciones en cada una de las coordenadas.
   float x;
