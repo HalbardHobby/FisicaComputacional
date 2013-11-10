524a1351f05e89f031ce6b311fe9571502b9bb04
Alejosp <mona@ubuntu.(none)>
2013-11-10 12:30:17 -0500

    hecho runge kutta y simucación con base en las constantes actuales.

--- Tarea6/code/masa.h
+++ Tarea6/code/masa.h
@@ -2,8 +2,8 @@
 #define MASA_H
 #include <math.h>
 
-const float C_GRAV=4.46391*pow(10,-4); // En 10E10 masas solares, gigayears, kiloparsecs
-const float M_CENT=100.0;//*pow(10,10); // En 10E10 masas solares 
+const float C_GRAV=4.46391*pow(10,-18); // En 10E10 masas solares, gigayears, kiloparsecs
+const float M_CENT=100.0*pow(10,10); // En 10E10 masas solares 
 
 typedef struct masa{//define una masa en el espacio.
   int id;//el identificador númerico de la masa.
